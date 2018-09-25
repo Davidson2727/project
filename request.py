4dc01d9b7284fedb798578788f2a88eafe077ac9
jak <davidson27@gmail.com>
2018-09-25 18:16:53 -0400

    notes

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 
 userdata = {"u_id":"0444234","u_name":"jasdfasdfasfde","u_pass":"12345","email":"address@email.com"}
-resp = requests.get('http://localhost/test.php', params=userdata)
+resp = requests.get('http://ddysfunction.ihostfull.com/test.php', params=userdata)
 
 print(resp.status_code)
 print('end')
