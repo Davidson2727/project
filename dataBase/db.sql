6f5d87aaa551d05cacdf56078b57a940530a9442
jak <davidson27@gmail.com>
2018-10-15 15:12:29 -0400

    add signup process

--- dataBase/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (userId int NOT NULL AUTO_INCREMENT,
-   userName varchar(255) NOT NULL,
-   password varchar(255) NOT NULL,
-   email varchar(255) NOT NULL,
-   PRIMARY KEY (userId));
-
-CREATE TABLE profiles(userId int NOT NULL,
-  FOREIGN KEY (userId) REFERENCES users(userId));
